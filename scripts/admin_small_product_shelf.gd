17d217f4818845e3b4719bacfde884a8df0fb58a
Dimitrigaming <dimitrijustin@yahoo.com>
2026-07-11 15:42:27 -0400

    Admin shelf spawns products from ProductSpawn node

--- scripts/admin_small_product_shelf.gd
+++ scripts/admin_small_product_shelf.gd
@@ -1,9 +1,21 @@
 extends "res://scripts/small_product_shelf.gd"
 
+const PRINT_MODEL_ADMIN = preload("res://models/default_model.tscn")
+
 var _spawn_timer: float = 0.0
 
 func _process(delta: float) -> void:
 	_spawn_timer += delta
 	if _spawn_timer >= 1.0:
 		_spawn_timer = 0.0
-		add_print("Default Model")
+		_spawn_print()
+
+func _spawn_print() -> void:
+	var zone = _first_available_zone("Default Model")
+	if zone == null:
+		return
+	var model = PRINT_MODEL_ADMIN.instantiate()
+	get_tree().current_scene.add_child(model)
+	var spawn = get_node_or_null("ProductSpawn")
+	model.global_position = spawn.global_position if spawn else global_position
+	add_print("Default Model", model)
