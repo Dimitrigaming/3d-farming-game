744af1f94037b333f0c9b6fed0cf06e1fe14e911
Dimitrigaming <dimitrijustin@yahoo.com>
2026-07-14 21:55:59 -0400

    Flood-fill nav links: short chain links between adjacent removed blocks

--- scripts/nav_link_manager.gd
+++ scripts/nav_link_manager.gd
@@ -2,7 +2,11 @@ extends Node3D
 
 const BLOCK_SIZE := 4.0
 
-var _star_links: Dictionary = {}  # block_key -> {link, target}
+# Entry links: NavLinkStart -> adjacent removed block
+var _entry_links: Dictionary = {}   # block_key -> NavigationLink3D
+# Chain links: between adjacent removed block pairs
+var _chain_links: Dictionary = {}   # "keyA-keyB" -> NavigationLink3D
+
 var _origin: Node3D
 
 func _ready() -> void:
@@ -15,58 +19,55 @@ func _ready() -> void:
 	_refresh_all()
 
 func _build_links() -> void:
-	for block in get_tree().get_nodes_in_group("interior_block"):
+	var blocks = get_tree().get_nodes_in_group("interior_block")
+	var pos_set: Dictionary = {}
+	for block in blocks:
+		pos_set[_key(block.global_position)] = block.global_position
+
+	for block in blocks:
 		var pos = block.global_position
 		var skey = _key(pos)
-		if not _star_links.has(skey):
-			_star_links[skey] = {
-				"link": _make_link(_origin.global_position, pos),
-				"target": pos,
-			}
+
+		# Entry link from NavLinkStart to this block (if adjacent to origin)
+		if not _entry_links.has(skey):
+			_entry_links[skey] = _make_link(_origin.global_position, pos)
+
+		# Chain links to each cardinal neighbor that also has a block
+		for dir in [Vector3(BLOCK_SIZE, 0, 0), Vector3(0, 0, BLOCK_SIZE)]:
+			var nb = pos + dir
+			var nkey = _key(nb)
+			if not pos_set.has(nkey):
+				continue
+			var lkey = _pair_key(skey, nkey)
+			if not _chain_links.has(lkey):
+				_chain_links[lkey] = _make_link(pos, nb)
 
 func _refresh_all() -> void:
-	var reachable = _flood_fill_reachable()
-	for skey in _star_links:
-		_star_links[skey]["link"].enabled = reachable.has(skey)
+	var blocks_in_scene: Dictionary = {}
+	for block in get_tree().get_nodes_in_group("interior_block"):
+		blocks_in_scene[_key(block.global_position)] = true
+
+	# Entry links: enable when block is removed AND adjacent to origin
+	for skey in _entry_links:
+		var pos = _get_entry_pos(skey)
+		var removed = not blocks_in_scene.has(skey)
+		var adjacent = _xz_dist(pos, _origin.global_position) < BLOCK_SIZE * 1.5
+		_entry_links[skey].enabled = removed and adjacent
+
+	# Chain links: enable when BOTH blocks are removed
+	for lkey in _chain_links:
+		var keys = lkey.split("-")
+		var both_removed = not blocks_in_scene.has(keys[0]) and not blocks_in_scene.has(keys[1])
+		_chain_links[lkey].enabled = both_removed
 
 func refresh_at(_removed_pos: Vector3) -> void:
 	await get_tree().process_frame
 	_refresh_all()
 
-# Flood-fill through removed block positions starting from NavLinkStart.
-# Only block positions that are contiguously connected (no walls between) are reachable.
-func _flood_fill_reachable() -> Dictionary:
-	var blocks_in_scene: Dictionary = {}
-	for block in get_tree().get_nodes_in_group("interior_block"):
-		blocks_in_scene[_key(block.global_position)] = true
-
-	# All positions that once had a block but no longer do
-	var removed: Dictionary = {}
-	for skey in _star_links:
-		if not blocks_in_scene.has(skey):
-			removed[skey] = _star_links[skey]["target"]
-
-	# Seed flood fill: removed blocks adjacent to NavLinkStart
-	var reachable: Dictionary = {}
-	var queue: Array = []
-	for skey in removed:
-		var pos: Vector3 = removed[skey]
-		if _xz_dist(pos, _origin.global_position) < BLOCK_SIZE * 1.5:
-			reachable[skey] = pos
-			queue.append(pos)
-
-	# Expand through adjacent removed blocks
-	while queue.size() > 0:
-		var current: Vector3 = queue.pop_front()
-		for dir in [Vector3(BLOCK_SIZE, 0, 0), Vector3(-BLOCK_SIZE, 0, 0),
-					Vector3(0, 0, BLOCK_SIZE), Vector3(0, 0, -BLOCK_SIZE)]:
-			var nb = current + dir
-			var nkey = _key(nb)
-			if removed.has(nkey) and not reachable.has(nkey):
-				reachable[nkey] = nb
-				queue.append(nb)
-
-	return reachable
+func _get_entry_pos(skey: String) -> Vector3:
+	if _entry_links.has(skey):
+		return to_global(_entry_links[skey].end_position)
+	return Vector3.ZERO
 
 func _xz_dist(a: Vector3, b: Vector3) -> float:
 	return Vector2(a.x, a.z).distance_to(Vector2(b.x, b.z))
@@ -82,3 +83,6 @@ func _make_link(from: Vector3, to: Vector3) -> NavigationLink3D:
 
 func _key(pos: Vector3) -> String:
 	return "%d,%d" % [roundi(pos.x), roundi(pos.z)]
+
+func _pair_key(a: String, b: String) -> String:
+	return a + "-" + b if a < b else b + "-" + a
