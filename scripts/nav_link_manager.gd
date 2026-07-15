729cbafae89e757c7ab09290beb51725264cc2d1
Dimitrigaming <dimitrijustin@yahoo.com>
2026-07-14 22:07:12 -0400

    Fix negative coord key bug in pair_key, use pipe separator

--- scripts/nav_link_manager.gd
+++ scripts/nav_link_manager.gd
@@ -2,10 +2,8 @@ extends Node3D
 
 const BLOCK_SIZE := 4.0
 
-# Entry links: NavLinkStart -> adjacent removed block
-var _entry_links: Dictionary = {}   # block_key -> NavigationLink3D
-# Chain links: between adjacent removed block pairs
-var _chain_links: Dictionary = {}   # "keyA-keyB" -> NavigationLink3D
+var _entry_links: Dictionary = {}  # block_key -> NavigationLink3D
+var _chain_links: Dictionary = {}  # "keyA|keyB" -> {link, key_a, key_b}
 
 var _origin: Node3D
 
@@ -28,11 +26,9 @@ func _build_links() -> void:
 		var pos = block.global_position
 		var skey = _key(pos)
 
-		# Entry link from NavLinkStart to this block (if adjacent to origin)
 		if not _entry_links.has(skey):
 			_entry_links[skey] = _make_link(_origin.global_position, pos)
 
-		# Chain links to each cardinal neighbor that also has a block
 		for dir in [Vector3(BLOCK_SIZE, 0, 0), Vector3(0, 0, BLOCK_SIZE)]:
 			var nb = pos + dir
 			var nkey = _key(nb)
@@ -40,34 +36,33 @@ func _build_links() -> void:
 				continue
 			var lkey = _pair_key(skey, nkey)
 			if not _chain_links.has(lkey):
-				_chain_links[lkey] = _make_link(pos, nb)
+				_chain_links[lkey] = {
+					"link": _make_link(pos, nb),
+					"key_a": skey,
+					"key_b": nkey,
+				}
 
 func _refresh_all() -> void:
 	var blocks_in_scene: Dictionary = {}
 	for block in get_tree().get_nodes_in_group("interior_block"):
 		blocks_in_scene[_key(block.global_position)] = true
 
-	# Entry links: enable when block is removed AND adjacent to origin
 	for skey in _entry_links:
-		var pos = _get_entry_pos(skey)
 		var removed = not blocks_in_scene.has(skey)
-		var adjacent = _xz_dist(pos, _origin.global_position) < BLOCK_SIZE * 1.5
-		_entry_links[skey].enabled = removed and adjacent
+		var link: NavigationLink3D = _entry_links[skey]
+		var block_pos = to_global(link.end_position)
+		var adjacent = _xz_dist(block_pos, _origin.global_position) < BLOCK_SIZE * 1.5
+		link.enabled = removed and adjacent
 
-	# Chain links: enable when BOTH blocks are removed
 	for lkey in _chain_links:
-		var keys = lkey.split("-")
-		var both_removed = not blocks_in_scene.has(keys[0]) and not blocks_in_scene.has(keys[1])
-		_chain_links[lkey].enabled = both_removed
+		var entry = _chain_links[lkey]
+		var both_removed = not blocks_in_scene.has(entry["key_a"]) \
+						and not blocks_in_scene.has(entry["key_b"])
+		entry["link"].enabled = both_removed
 
 func refresh_at(_removed_pos: Vector3) -> void:
 	await get_tree().process_frame
 	_refresh_all()
-
-func _get_entry_pos(skey: String) -> Vector3:
-	if _entry_links.has(skey):
-		return to_global(_entry_links[skey].end_position)
-	return Vector3.ZERO
 
 func _xz_dist(a: Vector3, b: Vector3) -> float:
 	return Vector2(a.x, a.z).distance_to(Vector2(b.x, b.z))
@@ -85,4 +80,4 @@ func _key(pos: Vector3) -> String:
 	return "%d,%d" % [roundi(pos.x), roundi(pos.z)]
 
 func _pair_key(a: String, b: String) -> String:
-	return a + "-" + b if a < b else b + "-" + a
+	return a + "|" + b if a < b else b + "|" + a
